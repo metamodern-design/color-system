55911c19ce9bf2a0fe2349df04131fa251ed175b
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-02 22:47:02 -0500

    sync

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-require = require('esm')(module);
-module.exports = require('./main.js');
+import bg from './src/bg';
+import colors from './src/colors';
+import grays from './src/grays';
+import lsh from './src/lsh';
+import text from './src/text';
+import theme from './src/theme';
+
+
+const colorSystem = (options = {}) => ({
+  bg: bg(options),
+  colors: colors(options),
+  grays: grays(options),
+  lsh: lsh(options),
+  text: text(options),
+  theme: theme(options),
+});
+
+
+export default colorSystem;
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import pkg from './package.json';
+
 
 export default [
   {
     input: 'index.js',
-    external: [
-    ],
+    external: [],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
-    ]
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+    ],
   },
 ];
