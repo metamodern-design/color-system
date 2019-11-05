a51e1dd81b20084d48fe8ac1f1b8334a291b3a0a
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-05 10:04:59 -0500

    sync

---
+++ test/dist.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+import module from '../dist/module';
+import main from '../dist/main';
+
+
+const run = (bundle) => {
+  const theme = bundle().theme;
+
+  test('theme with default options', (t) => {
+    t.is(
+      theme['h30-s55-l62'],
+      'hsl(30,55%,62%)',
+    );
+    t.is(
+      theme['h255-s25-l12'],
+      'hsl(255,25%,12%)',
+    );
+    t.is(
+      theme['h315-s85-l77'],
+      'hsl(315,85%,77%)',
+    );
+  });
+};
+
+
+run(module);
+run(main);
