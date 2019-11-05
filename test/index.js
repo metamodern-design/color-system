b1ac040fcd3be330e6493dd3b9648f21aaddfbc8
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-05 00:21:50 -0500

    sync

---
+++ test/index.js
@@ -0,0 +1,19 @@
+import test from 'ava';
+import shades from '../src/shades';
+import tones from '../src/tones';
+
+
+test('shades', (t) => {
+  t.deepEqual(
+    shades({ start: 0, stop: 100, step: 25 }),
+    [0, 25, 50, 75, 100],
+  );  
+});
+
+
+test('tones', (t) => {
+  t.deepEqual(
+    tones({ start: 5, stop: 95, step: 20 }),
+    [5, 25, 45, 65, 85],
+  );  
+});
