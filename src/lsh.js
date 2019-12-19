4110d52b6e94e300b19f1a6d484edc7f93fb076f
danielnarey <accounts@danielnarey.com>
2019-12-19 14:15:54 +0000

    automated sync

--- src/lsh.js
+++ src/lsh.js
@@ -5,13 +5,13 @@ const lsh = (options = {}) => (l = 62) => (s = 0) => (h) => {
   if (!h && h !== 0) {
     return `hsl(0,0%,${l}%)`;
   }
-  
+
   const namedHue = named(options).get(h);
 
   if (!namedHue && namedHue !== 0) {
     return `hsl(${h},${s}%,${l}%)`;
   }
-  
+
   return `hsl(${namedHue},${s}%,${l}%)`;
 };
 
