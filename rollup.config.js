cbeae2514038850f2ce6b4f9796575f1e71c8833
Daniel Narey <danielnarey@users.noreply.github.com>
2020-06-03 22:54:31 -0400

    restore commonjs support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ export default [
   {
     input: 'src/index.js',
     external: [],
-    output: [{ file: 'dist/index.js', format: 'es' }],
+    output: [
+      { file: 'dist/index.js', format: 'es' },
+      { file: 'dist/main.cjs', format: 'cjs' },
+    ],
   },
 ];
